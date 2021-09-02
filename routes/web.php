29a51826648cceafaeeeab993c30461bc952c31a
Ajibola03 <atoyebieniola93@gmail.com>
2021-09-02 15:31:22 +0100

    code clean up

--- routes/web.php
+++ routes/web.php
@@ -20,15 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/plugin-info', [PluginInfoController::class, 'servePluginInfo']);
 Route::get('/create-todo', [TodoController::class, 'showPage'])->name('show.create-todo');
 Route::post('/create-todo', [TodoController::class, 'store'])->name('create-todo');
-
-
-
-Route::get('/todo', [TodoController::class, 'index'])->name('get-todo');
-
 Route::get('/ping', function () {
     return response()->json(['message' => 'Server is Live!'], 200);
 });
